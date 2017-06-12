0a9f6559ae966eb66263faec472cc88da52ccdd0
JJ Kavelaars <jjk@uvic.ca>
2017-06-11 21:43:18 -0700

    Added ability to load a series of images and display using the f/g commands.
    We need to get some async downloads going

--- src/daomop/viewer.py
+++ src/daomop/viewer.py
@@ -1,8 +1,13 @@
 from ginga.web.pgw import ipg
-from daomop import storage, wcs
+import storage
 from astropy.wcs import WCS
+import wcs
 import mp_ephem
-from cadcutils.net import get_cert
+import logging
+
+WCS.all_pix2world = wcs.WCS.all_pix2world
+
+logging.basicConfig(level=logging.INFO)
 
 
 class Viewer(object):
@@ -20,6 +25,8 @@ class Viewer(object):
         self.server = ipg.make_server(host='localhost', port=9914, use_opencv=False)
         self.server.start(no_ioloop=True)
         self.viewer = self.server.get_viewer('v1')
+        self.viewer.enable_autocuts('on')
+        self.viewer.set_autocut_params('zscale')
         self.viewer.open()
 
         # creating drawing canvas; initializing polygon types
@@ -33,16 +40,17 @@ class Viewer(object):
         # reading target file; setting database location; creating objects
         storage.DBIMAGES = dbimages
         self.obs = mp_ephem.EphemerisReader().read(target)
+        self._loaded_images = [None] * len(self.obs)
+        logging.info("Computing preliminary orbit.")
         self.orb = mp_ephem.BKOrbit(self.obs)
-
+        logging.info(self.orb.summarize())
+        self.obs_number = 0
         self.image = None
         self.image_values = None
-        self.obs_number = None
 
         # eventually store different HDU's for image switching,
-        #  probably will be replaced with a single index counter variable in _key_press
-        self._obs_0 = None
-        self._obs_1 = None
+        # probably will be replaced with a single index counter variable in _key_press
+        self._current_image = None
 
     def load(self, obs_number=0):
         """
@@ -52,33 +60,19 @@ class Viewer(object):
         :param obs_number: index of which line in the file gets loaded/displayed in the viewer
         """
         self.obs_number = obs_number
-
-        self.image = storage.FitsImage.from_frame(self.obs[self.obs_number].comment.frame)
+        self._load()
 
         # Attempt to monkey patch all_pix2world, doesn't seem to be getting called before keyword error is thrown.
         # Warnings probably being thrown before pix2world is called.
-        WCS.all_pix2world = wcs.WCS.all_pix2world
-
-        hdu = self.image.ra_dec_cutout(self.obs[self.obs_number].coordinate)[2]  # always index 2?
-
-        self.viewer.load_hdu(hdu)
-        self._mark_aperture()
-        self._predict_obs_record()
 
     def _mark_aperture(self):
         """
         Draws a red circle on the drawing canvas in the viewing window around the celestial object
          being observed.
         """
-        # move to load method^ ?
-        self.image_values = self.viewer.get_image()  # getting values from the image (from ginga example notebook)
-
-        # x, y = self.obs[self.obs_number].coordinate.ra.deg, self.obs[self.obs_number].coordinate.dec.deg
-        # tmp = self.image_values.radectopix(x, y)  # can't convert WCS into x/y for drawing circle
-
-        tmp = (264, 284)  # tmp should be a 2-tuple of floats, hard coded values for now
-        x, y = tmp[0], tmp[1]
-
+        ra, dec = self.obs[self.obs_number].coordinate.ra, self.obs[self.obs_number].coordinate.dec
+        x, y = wcs.WCS(self._loaded_images[self.obs_number].header).sky2xy(ra, dec, usepv=True)
+        self.canvas.deleteAllObjects()
         self.canvas.add(self.circle(x, y, radius=10, color='red'))
 
     def _predict_obs_record(self):
@@ -122,6 +116,7 @@ class Viewer(object):
         :param opn: str "key"
         :param viewer: Ginga EnhancedCanvasView object
         """
+        logging.debug("Got key: {} from canvas: {} with opn: {} from viewer: ".format(canvas, keyname, opn, viewer))
         if keyname == 'f':  # index 0
             self.viewer.set_onscreen_message("pressed f")  # TODO: remove onscreen messages once finished implementation
 
@@ -136,34 +131,42 @@ class Viewer(object):
             #  Probably don't need any warning, but it would be possible to have an onscreen message pop up.
             # Retrieving the image cutouts definitely takes more time than needed, not sure if anything in storage.py
             #  can be sped up or if it's a server issue.
-            if self._obs_0 is None:
-                self._obs_0 = self._load()
-
-            else:
-                self.viewer.load_hdu(self._obs_0)
-
+            self.obs_number += 1
         elif keyname == 'g':
-            self.viewer.set_onscreen_message("pressed g")
+            self.obs_number -= 1
+        else:
+            logging.warning("Unknown keystroke {}".format(keyname))
+            return
 
-            if self._obs_1 is None:
-                self._obs_1 = self._load(1)
+        self.obs_number %= len(self.obs)
+        self._load()
 
-            else:
-                self.viewer.load_hdu(self._obs_1)
-
-    def _load(self, obs_number=0):
+    def _load(self):
         """
-        Protected method similar to load but doesn't draw anything on the canvas. Gets the image hdu, loads it
-         into the viewer, and returns the cutout HDU so it can be saved as a variable in _key_press.
-
-        :param obs_number: index of which line in the file gets loaded/displayed in the viewer
-        :return hdu: ImageHDU object which can be quickly loaded into the viewer
+        Checks if an HDU has been loaded already and retrieves if needed and then displays that HDU.
         """
-        image = storage.FitsImage.from_frame(self.obs[obs_number].comment.frame)
-        hdu = image.ra_dec_cutout(self.obs[obs_number].coordinate)[2]  # always index 2?
-        self.viewer.load_hdu(hdu)
+        # load the image if not already available, for now we'll put this in here.
+        self.viewer.clear()
+        if self._loaded_images[self.obs_number] is None:
+            self.get_hdu(self.obs_number)
+        self.viewer.load_hdu(self._loaded_images[self.obs_number])
+        self._mark_aperture()
+        self.viewer.set_onscreen_message("Loaded: {}".format(self.obs[self.obs_number].comment.frame))
 
-        return hdu
+    def get_hdu(self, obs_number):
+        """
+        Retrieve a fits image associated with a given obs_record and return the HDU off the assocaited cutout.
+
+        :param obs_number: the number of the record to retrieve.
+        :type obs_number: int
+        :return:
+        """
+        obs_record = self.obs[obs_number]
+        image = storage.FitsImage.from_frame(obs_record.comment.frame)
+        hdu = image.ra_dec_cutout(obs_record.coordinate)[-1]  # always index 2?
+        self._loaded_images[obs_number] = hdu
+        logging.info("Retrieved {} and stored {}".format(obs_number, self._loaded_images))
+        return
 
     def __del__(self):
         self.server.stop()
