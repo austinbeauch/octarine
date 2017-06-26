7eb6b72190ba4bd299e89ce831ccf508f897d149
Austin Beauchamp <austinbeauch@gmail.com>
2017-06-26 15:32:25 -0700

    Move control methods into ValidateGui

--- src/daomop/downloader.py
+++ src/daomop/downloader.py
@@ -1,3 +1,45 @@
+import logging
 import storage
-import multiprocessing
 
+
+class Downloader(object):
+    def __init__(self):
+        self._downloaded_images = dict()
+        self.getter_lock = {}
+
+    @staticmethod
+    def image_key(obs_record):
+        """
+
+        :param obs_record: the observation record to get the key for.
+        :type obs_record: mp_ephem.ObsRecord
+        :return: the key for the given observation record.
+        """
+        return obs_record.comment.frame + obs_record.provisional_name
+
+    def get(self, obs_record):
+        if self.image_key(obs_record) not in self._downloaded_images:
+            if obs_record in self.getter_lock:
+                self.getter_lock[obs_record].wait(timeout=10)
+            else:
+                self._downloaded_images[self.image_key(obs_record)] = self.get_hdu(obs_record)
+        return self._downloaded_images[self.image_key(obs_record)]
+
+    def get_hdu(self, obs_record):
+        """
+        Retrieve a fits image associated with a given obs_record and return the HDU off the assocaited cutout.
+
+        :param obs_record: the ObsRecord for which the image is to be retrieved
+        :type obs_record: mp_ephem.ObsRecord
+        :return: fits.ImageHDU
+        """
+        logging.info("Retrieving {}".format(self.image_key(obs_record)))
+        try:
+            image = storage.FitsImage.from_frame(obs_record.comment.frame)
+            hdu = image.ra_dec_cutout(obs_record.coordinate)[-1]
+        except Exception as ex:
+            logging.info(ex)
+            raise ex
+
+        logging.info("Got {}".format(hdu))
+        return hdu
