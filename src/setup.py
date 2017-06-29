49667a134fee8365259c0e3405898ca6eeb752a4
JJ Kavelaars <jjk@uvic.ca>
2017-06-29 09:03:48 -0700

    Add canfar_job to console scripts and make scripts have package style names
    Package style names have the name of the package (daomop) prepended to the command name.
    This helps prevent name collisions in the executable path.  But the names are built inside setup.py
    not inside the source repo.

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,5 @@
-import os
 import sys
 from daomop.__version__ import version
-
 from setuptools import setup, find_packages
 
 dependencies = ['requests >= 2.7',
@@ -19,24 +17,19 @@ if sys.version_info[0] > 2:
     print 'The MOP package is only compatible with Python version 2.7+, not yet with 3.x'
     sys.exit(-1)
 
-# # Build the list of validate and scripts to be installed.
-#script_dirs = ['validate', 'scripts']
-#scripts = []
-#for script_dir in script_dirs:
-#    for script in os.listdir(script_dir):
-#        if script[-1] in ["~", "#"]:
-#           continue
-#        scripts.append(os.path.join(script_dir, script))
-
-console_scripts = [ 'populate = daomop.populate:main', 'stationary = daomop.stationary:main', 'build_cat = daomop.build_cat:main', 
-                     'hpx_map = daomop.hpx_map:main']
+console_scripts = ['daomop_populate = daomop.populate:main',
+                   'daomop_stationary = daomop.stationary:main',
+                   'daomop_cat = daomop.build_cat:main',
+                   'daomop_canfar_job = daomop.canfar_job:main',
+                   'hpx_map = daomop.hpx_map:main']
 
 setup(name='daomop',
       version=version,
       url='http://github.com/ijiraq/daomop',
       author='''JJ Kavelaars (jjk@uvic.ca),
-              Michele Bannister (micheleb@uvic.ca)''',
-      maintainer='M Bannister and JJ Kavelaars',
+              Michele Bannister (micheleb@uvic.ca),
+              Austin Beauchamp (austinbeauch@gmail.com)''',
+      maintainer='JJ Kavelaars',
       maintainer_email='jjk@uvic.ca',
       description="Dominion Astrophysical Observatory Moving Object Pipeline: daomop",
       long_description='See github repp',
@@ -49,7 +42,7 @@ setup(name='daomop',
                    'License :: OSI Approved :: GNU General Public License (GPL)',
                    ],
       install_requires=dependencies,
-      entry_points = { 'console_scripts': console_scripts},
-      packages=find_packages(exclude=['tests',]),
-      package_data={'daomop': ['config/*' ]}
+      entry_points={'console_scripts': console_scripts},
+      packages=find_packages(exclude=['tests']),
+      package_data={'daomop': ['config/*']}
       )
