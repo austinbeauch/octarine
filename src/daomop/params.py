5ca5a1af273ccd429cc62b6792cc1da08890603b
JJ Kavelaars <jjk@uvic.ca>
2017-06-30 07:37:00 -0700

    Make stationary qrunid based but utilize data from all qruns.
    The stationary catalogs should be based on the checking if a source appeared in any exposure take so far, regardless of qrun.
    The catalogs themselves are qrun based.

--- src/daomop/params.py
+++ src/daomop/params.py
@@ -7,5 +7,26 @@ CFHT_QRUNS = {
     '17AQ08': (Time('2017-04-18 22:00:00'), Time('2017-05-03 22:00:00')),
     '17AQ10': (Time('2017-05-16 22:00:00'), Time('2017-05-30 22:00:00')),
     '17AQ13': (Time('2017-06-15 22:00:00'), Time('2017-06-27 22:00:00')),
-    '17AQ16': (Time('2017-07-14 22:00:00'), Time('2017-07-31 22:00:00'))
+    '17AQ16': (Time('2017-07-14 22:00:00'), Time('2017-07-31 22:00:00')),
+    'default': (Time('2017-01-01 00:00:00'), Time('2021-01-01 00:00:00'))
 }
+
+
+def qrunid_start_date(qrunid):
+    """
+
+    :param qrunid: CFHT QRUN to give the start date of
+    :return: start date of QRUN
+    :rtype: Time
+    """
+    return CFHT_QRUNS.get(qrunid, CFHT_QRUNS['default'])[0]
+
+
+def qrunid_end_date(qrunid):
+    """
+
+    :param qrunid: CFHT QRUN to give the end date of
+    :return: end date of QRUN
+    :rtype: Time
+    """
+    return CFHT_QRUNS.get(qrunid, CFHT_QRUNS['default'])[1]
