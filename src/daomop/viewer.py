969ba1d005bdbe40c3595e001c60c90ff11b81ea
Austin Beauchamp <austinbeauch@gmail.com>
2017-07-18 09:06:52 -0700

    Fix key error from astro_images
    An image that hasn't been rendered won't be in astro_images, so
    deleting the key will throw an error. Since these dictionaries
    are cleared so often, the lookup time should be negligible.

--- src/daomop/viewer.py
+++ src/daomop/viewer.py
@@ -482,6 +482,10 @@ class ValidateGui(ipg.EnhancedCanvasView):
             self.console_box.append_text('Reloading all candidates...\n')
             self.pool.terminate()
             self.pool = Pool(processes=PROCESSES)
+            self.next_set.set_enabled(True)
+            self.previous_set.set_enabled(True)
+            self.accept.set_enabled(True)
+            self.reject.set_enabled(True)
             self.load_candidates(self.pixel)
             self.next()
 
@@ -697,11 +701,13 @@ class ValidateGui(ipg.EnhancedCanvasView):
         for obs_record in self.candidate:
             key = self.downloader.image_key(obs_record)
             del(self.image_list[key])
-            del(self.astro_images[key])
+            if key in self.astro_images:
+                del(self.astro_images[key])
             if key in self.comparison_images:
                 comp_key = self.comparison_images[key]
                 del(self.image_list[comp_key])
-                del(self.astro_images[comp_key])
+                if comp_key in self.astro_images:
+                    del(self.astro_images[comp_key])
 
     @property
     def center(self):
