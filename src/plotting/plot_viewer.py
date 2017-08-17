bceaea17888943e5c3ed5b0e0548e4a79de92b71
Austin Beauchamp <austinbeauch@gmail.com>
2017-08-17 14:40:31 -0700

    Allow sequential viewing of all images by entering nothing

--- src/plotting/plot_viewer.py
+++ src/plotting/plot_viewer.py
@@ -6,6 +6,45 @@ from astropy.io import fits
 from astropy import wcs
 
 
+def load_images(hpx, hpx_files):
+    qrun = None
+    for filename in hpx_files:
+        x = re.match('(?P<number>\d{3,5})_17AQ(?P<qrun>\d{2})', filename)
+        if qrun is None:
+            qrun = '17AQ' + x.group('qrun')
+
+        if hpx in filename and qrun in filename and 'density' in filename:
+            hdu = fits.open('./fits_data/' + filename)[0]
+            w = wcs.WCS(hdu.header)
+            plt.subplot(313, projection=w)
+            plt.imshow(hdu.data, origin='lower', cmap='binary')
+            plt.title(x.group('number') + ' ' + qrun + ' Stellar Densities')
+            plt.xlabel('RA')
+            plt.ylabel('Dec')
+
+        elif hpx in filename and qrun in filename and '_mag_' in filename:
+            hdu = fits.open('./fits_data/' + filename)[0]
+            w = wcs.WCS(hdu.header)
+            plt.subplot(311, projection=w)
+            plt.imshow(hdu.data, origin='lower', cmap='viridis', vmin=22)
+            plt.title(x.group('number') + ' ' + qrun + ' Magnitudes')
+            plt.xlabel('RA')
+            plt.ylabel('Dec')
+
+        elif hpx in filename and qrun in filename and 'overlap' in filename:
+            hdu = fits.open('./fits_data/' + filename)[0]
+            w = wcs.WCS(hdu.header)
+            plt.subplot(312, projection=w)
+            plt.imshow(hdu.data, origin='lower', cmap='binary')
+            plt.title(x.group('number') + ' ' + qrun + ' Overlaps')
+            plt.xlabel('RA')
+            plt.ylabel('Dec')
+            plt.show()  # overlap file comes last in the directory, wait to show plots until it's reached
+            qrun = None
+        else:
+            qrun = None
+
+
 def main():
     directory = sorted(os.listdir('./fits_data/'))
     directory_names = []
@@ -20,52 +59,23 @@ def main():
         for i in sorted(directory_names):
             print i,
         print
-        print "Type 'exit' to quit"
+        print "Hit <enter> key to view all, or type 'exit' to quit."
         hpx = raw_input("Enter HPX to display: ")
+
+        hpx_files = []
+        for filename in directory:
+            if hpx in filename:
+                hpx_files.append(filename)
 
         if hpx == 'exit':
             sys.exit(0)
+
+        if hpx == '':
+            for i in sorted(directory_names):
+                load_images(str(i), hpx_files)
+
         else:
-            hpx_files = []
-            for filename in directory:
-                if hpx in filename:
-                    hpx_files.append(filename)
-            qrun = None
-            for filename in hpx_files:
-                x = re.match('(?P<number>\d{3,5})_17AQ(?P<qrun>\d{2})', filename)
-                if qrun is None:
-                    qrun = '17AQ' + x.group('qrun')
-
-                if hpx in filename and qrun in filename and 'density' in filename:
-                    hdu = fits.open('./fits_data/' + filename)[0]
-                    w = wcs.WCS(hdu.header)
-                    plt.subplot(313, projection=w)
-                    plt.imshow(hdu.data, origin='lower', cmap='binary')
-                    plt.title(x.group('number') + ' ' + qrun + ' Stellar Densities')
-                    plt.xlabel('RA')
-                    plt.ylabel('Dec')
-
-                elif hpx in filename and qrun in filename and '_mag_' in filename:
-                    hdu = fits.open('./fits_data/' + filename)[0]
-                    w = wcs.WCS(hdu.header)
-                    plt.subplot(311, projection=w)
-                    plt.imshow(hdu.data, origin='lower', cmap='viridis', vmin=22)
-                    plt.title(x.group('number') + ' ' + qrun + ' Magnitudes')
-                    plt.xlabel('RA')
-                    plt.ylabel('Dec')
-
-                elif hpx in filename and qrun in filename and 'overlap' in filename:
-                    hdu = fits.open('./fits_data/' + filename)[0]
-                    w = wcs.WCS(hdu.header)
-                    plt.subplot(312, projection=w)
-                    plt.imshow(hdu.data, origin='lower', cmap='binary')
-                    plt.title(x.group('number') + ' ' + qrun + ' Overlaps')
-                    plt.xlabel('RA')
-                    plt.ylabel('Dec')
-                    plt.show()  # overlap file comes last in the directory, wait to show plots until it's reached
-                    qrun = None
-                else:
-                    qrun = None
+            load_images(hpx, hpx_files)
 
 
 if __name__ == '__main__':
