d99667d43147b6612d77bb7da5ff0274037a1045
Austin Beauchamp <austinbeauch@gmail.com>
2017-08-18 11:03:22 -0700

    Encompass entire fits_factory in a try/catch statement

--- src/plotting/mag_data.py
+++ src/plotting/mag_data.py
@@ -23,112 +23,116 @@ class IDX(object):
 
 
 def fits_factory(hpx):
-    print hpx
+    try:
+        print hpx
 
-    cat = storage.HPXCatalog(hpx, catalog_dir='catalogs/master', dest_directory='master')
-    table = cat.table
+        cat = storage.HPXCatalog(hpx, catalog_dir='catalogs/master', dest_directory='master')
+        table = cat.table
 
-    table['frame'] = [dataset_name.split('p')[0] for dataset_name in table['dataset_name']]
-    dataset_names = np.unique(table['frame'])
-    table_entries = [table[table['frame'] == name] for name in dataset_names]
+        table['frame'] = [dataset_name.split('p')[0] for dataset_name in table['dataset_name']]
+        dataset_names = np.unique(table['frame'])
+        table_entries = [table[table['frame'] == name] for name in dataset_names]
 
-    mag_lims = {}
+        mag_lims = {}
 
-    for table_entry in table_entries:
-        previous_mag = None
-        for mag in np.arange(table_entry['MAG_AUTO'].min(), table_entry['MAG_AUTO'].max(), 0.20):
-            condition = np.all((table_entry['MAG_AUTO'] > mag, table_entry['MAG_AUTO'] <= mag + 0.2), axis=0)
-            magerr = np.median(table_entry['MAGERR_AUTO'][condition])
-            if magerr > 0.15:
-                if previous_mag is None:
-                    previous_mag = mag
+        for table_entry in table_entries:
+            previous_mag = None
+            for mag in np.arange(table_entry['MAG_AUTO'].min(), table_entry['MAG_AUTO'].max(), 0.20):
+                condition = np.all((table_entry['MAG_AUTO'] > mag, table_entry['MAG_AUTO'] <= mag + 0.2), axis=0)
+                magerr = np.median(table_entry['MAGERR_AUTO'][condition])
+                if magerr > 0.15:
+                    if previous_mag is None:
+                        previous_mag = mag
+                    else:
+                        frame = table_entry['frame'][0].split('p')[0]
+                        mag_lims[frame] = mag
+                        break
                 else:
-                    frame = table_entry['frame'][0].split('p')[0]
-                    mag_lims[frame] = mag
-                    break
-            else:
-                previous_mag = None
+                    previous_mag = None
 
-    ra_idx = IDX(table['X_WORLD'].min(), table['X_WORLD'].max(), PIXEL_WIDTH)
-    dec_idx = IDX(table['Y_WORLD'].min(), table['Y_WORLD'].max(), PIXEL_WIDTH)
+        ra_idx = IDX(table['X_WORLD'].min(), table['X_WORLD'].max(), PIXEL_WIDTH)
+        dec_idx = IDX(table['Y_WORLD'].min(), table['Y_WORLD'].max(), PIXEL_WIDTH)
 
-    mag_data = np.zeros((len(dec_idx), len(ra_idx)))
-
-    w = wcs.WCS(naxis=2)
-    w.wcs.cd = [[PIXEL_WIDTH, 0], [0, PIXEL_WIDTH]]
-    crpix1 = mag_data.shape[1]/2
-    crpix2 = mag_data.shape[0]/2
-    w.wcs.crpix = [crpix1, crpix2]
-    crval = [ra_idx.centre, dec_idx.centre]
-    w.wcs.crval = crval
-    w.wcs.ctype = ['ra--tan', 'dec-tan']
-    w.wcs.cunit = ['deg', 'deg']
-    header = w.to_header()
-
-    print mag_data.shape[1], mag_data.shape[0]
-    for qrun in np.unique(table['QRUNID']):
-
-        # set file names and reset data arrays for each qrunid
-        mag_image_filename = FITS_DIRECTORY + str(hpx) + '_' + qrun + '_mag_data.fits'
-        overlap_image_filename = FITS_DIRECTORY + str(hpx) + '_' + qrun + '_overlap_image.fits'
-        density_image_filename = FITS_DIRECTORY + str(hpx) + '_' + qrun + '_density_image.fits'
         mag_data = np.zeros((len(dec_idx), len(ra_idx)))
-        overlap_data = np.zeros((len(dec_idx), len(ra_idx)))
-        stellar_density_data = np.zeros((len(dec_idx), len(ra_idx)))
-        print qrun, mag_image_filename
 
-        if not os.path.exists(density_image_filename):
-            count = 0
-            for xx in range(mag_data.shape[1]):
-                print count
-                count += 1
+        w = wcs.WCS(naxis=2)
+        w.wcs.cd = [[PIXEL_WIDTH, 0], [0, PIXEL_WIDTH]]
+        crpix1 = mag_data.shape[1]/2
+        crpix2 = mag_data.shape[0]/2
+        w.wcs.crpix = [crpix1, crpix2]
+        crval = [ra_idx.centre, dec_idx.centre]
+        w.wcs.crval = crval
+        w.wcs.ctype = ['ra--tan', 'dec-tan']
+        w.wcs.cunit = ['deg', 'deg']
+        header = w.to_header()
 
-                for yy in range(mag_data.shape[0]):
-                    ra, dec = w.all_pix2world(xx, yy, 0)
+        print mag_data.shape[1], mag_data.shape[0]
+        for qrun in np.unique(table['QRUNID']):
 
-                    ra_cond = np.all((table['X_WORLD'] >= ra,
-                                      table['X_WORLD'] < ra + PIXEL_WIDTH,
-                                      table['OVERLAPS'] > 1,
-                                      table['QRUNID'] == qrun),
-                                     axis=0)
+            # set file names and reset data arrays for each qrunid
+            mag_image_filename = FITS_DIRECTORY + str(hpx) + '_' + qrun + '_mag_data.fits'
+            overlap_image_filename = FITS_DIRECTORY + str(hpx) + '_' + qrun + '_overlap_image.fits'
+            density_image_filename = FITS_DIRECTORY + str(hpx) + '_' + qrun + '_density_image.fits'
+            mag_data = np.zeros((len(dec_idx), len(ra_idx)))
+            overlap_data = np.zeros((len(dec_idx), len(ra_idx)))
+            stellar_density_data = np.zeros((len(dec_idx), len(ra_idx)))
+            print qrun, mag_image_filename
 
-                    dec_cond = np.all((ra_cond,
-                                       table['Y_WORLD'] >= dec,
-                                       table['Y_WORLD'] < dec + PIXEL_WIDTH),
-                                      axis=0)
+            if not os.path.exists(density_image_filename):
+                count = 0
+                for xx in range(mag_data.shape[1]):
+                    print count
+                    count += 1
 
-                    dataset_names = np.unique(table[dec_cond]['frame'])
+                    for yy in range(mag_data.shape[0]):
+                        ra, dec = w.all_pix2world(xx, yy, 0)
 
-                    if len(dataset_names) != 0:
-                        density = float(len(table[dec_cond])) / len(dataset_names)
-                        stellar_density_data[yy, xx] = density
+                        ra_cond = np.all((table['X_WORLD'] >= ra,
+                                          table['X_WORLD'] < ra + PIXEL_WIDTH,
+                                          table['OVERLAPS'] > 1,
+                                          table['QRUNID'] == qrun),
+                                         axis=0)
 
-                        if len(dataset_names) > 2:
-                            l = []
-                            try:
-                                for frame in dataset_names:
-                                    l.append(float(mag_lims[frame]))  # getting third faintest magnitude
-                                third_faintest = sorted(l)[-3]
-                                print 'index: ({} , {})'.format(yy, xx)
-                                mag_data[yy, xx] = third_faintest
-                                overlap_data[yy, xx] = len(dataset_names)
-                            except KeyError:
-                                print KeyError
-                                continue
+                        dec_cond = np.all((ra_cond,
+                                           table['Y_WORLD'] >= dec,
+                                           table['Y_WORLD'] < dec + PIXEL_WIDTH),
+                                          axis=0)
 
-            for row in mag_data:
-                if row.any() != 0:
-                    # only write these files if there's data that has been inserted
-                    mag_image = fits.PrimaryHDU(data=mag_data, header=header)
-                    mag_image.writeto(mag_image_filename)
+                        dataset_names = np.unique(table[dec_cond]['frame'])
 
-                    overlap_image = fits.PrimaryHDU(data=overlap_data, header=header)
-                    overlap_image.writeto(overlap_image_filename)
-                    break
+                        if len(dataset_names) != 0:
+                            density = float(len(table[dec_cond])) / len(dataset_names)
+                            stellar_density_data[yy, xx] = density
 
-            # write the stellar density image regardless so it's not regathered again in case of empty mag_data
-            density_image = fits.PrimaryHDU(data=stellar_density_data, header=header)
-            density_image.writeto(density_image_filename)
+                            if len(dataset_names) > 2:
+                                l = []
+                                try:
+                                    for frame in dataset_names:
+                                        l.append(float(mag_lims[frame]))  # getting third faintest magnitude
+                                    third_faintest = sorted(l)[-3]
+                                    print 'index: ({} , {})'.format(yy, xx)
+                                    mag_data[yy, xx] = third_faintest
+                                    overlap_data[yy, xx] = len(dataset_names)
+                                except KeyError:
+                                    print KeyError
+                                    continue
+
+                for row in mag_data:
+                    if row.any() != 0:
+                        # only write these files if there's data that has been inserted
+                        mag_image = fits.PrimaryHDU(data=mag_data, header=header)
+                        mag_image.writeto(mag_image_filename)
+
+                        overlap_image = fits.PrimaryHDU(data=overlap_data, header=header)
+                        overlap_image.writeto(overlap_image_filename)
+                        break
+
+                # write the stellar density image regardless so it's not regathered again in case of empty mag_data
+                density_image = fits.PrimaryHDU(data=stellar_density_data, header=header)
+                density_image.writeto(density_image_filename)
+    except Exception as ex:
+        print ex
+        return
 
 
 def main():
@@ -141,7 +145,7 @@ def main():
 
         hpx = int(x.group('pix'))
 
-        if hpx not in reg and hpx > 0:  # alter 'hpx > 0' to use specific files
+        if hpx not in reg and hpx > 1500:  # alter 'hpx > 0' to use specific files
             reg.append(hpx)
             fits_factory(hpx)
 
